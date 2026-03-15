979bd965e2c4914590783253226d711929986ec6
sufster <yusalie@gmail.com>
2026-03-15 08:50:43 +0200

    updated

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
-    clerkID:{
+    clerkId:{
         type:String,
         required:true,
         unique:true,
