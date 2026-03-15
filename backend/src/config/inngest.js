f03143e1a07593318719c1fbde5399f7fe55b1fa
sufster <yusalie@gmail.com>
2026-03-15 09:21:43 +0200

    update

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -28,18 +28,14 @@ const syncUser = inngest.createFunction(
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  {
-    id: "delete-user-from-db",
-  },
-  {
-    event: "clerk/user.deleted",
-  },
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
   async ({ event }) => {
+    await connectDB();
+
     const { id } = event.data;
 
     await User.deleteOne({ clerkId: id });
-
-    await deleteStreamUser(id.toString());
   }
 );
 
